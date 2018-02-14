07d58d47f006ef0d4b8bf485ee0c4105a7e48df9
Sihong Huang <shuang@ny-057.thecementworks.com>
2018-02-14 14:46:51 -0500

    angular practice

--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'no server was created';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver2'];
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
