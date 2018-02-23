d48cb9c8deba05c49261a492f8b4aae126f13821
Sihong Huang <shuang@ny-057.thecementworks.com>
2018-02-23 16:52:03 -0500

    angular practice

---
+++ observables-start/src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      } ,2000);
+      setTimeout(() => {
+        observer.next('second package');
+      } ,4000);
+      setTimeout(() => {
+        observer.complete();
+      } ,5000);
+      setTimeout(() => {
+        observer.next('third package');
+      } ,6000);
+      setTimeout(() => {
+        observer.error('this does not work');
+      } ,8000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data) },
+      (error: string) => { console.log(error) },
+      () => { console.log('completed') }
+    );
+
+  }
+
+}
