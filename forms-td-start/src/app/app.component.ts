395bc4b917fe9f655ade1a298b78ddd3432382c1
sihong31 <sihongspring2011@gmail.com>
2018-02-25 23:51:05 -0500

    angular practice

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
