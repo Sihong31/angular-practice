3242dbeaf2c07687265628b04431fddbafff7a96
sihong31 <sihongspring2011@gmail.com>
2018-02-27 23:43:08 -0500

    angular practice

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,30 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value);
+    // )
+    // this.signupForm.statusChanges.subscribe(
+    //   (status) => console.log(status);
+    // )
+    // this.signupForm.setValue({
+    //   'userData': {
+    //     'username': 'Max',
+    //     'email':'Max@test.com'
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    // })
+    // this.signupForm.patchValue({
+    //   'userData': {
+    //     'username': 'Anna',
+    //   },
+    // })
   }
 
   forbiddenNames(control: FormControl) : {[s: string]: boolean} {
@@ -29,6 +49,19 @@ export class AppComponent implements OnInit {
     return null;
   }
 
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+    setTimeout(() => {
+      if (control.value === 'test@test.com') {
+        resolve({'emailIsForbidden': true});
+      } else {
+        resolve(null);
+      }
+     }, 1500);
+    });
+    return promise;
+  }
+
   onAddHobby() {
       const control = new FormControl(null, Validators.required);
       (<FormArray>this.signupForm.get('hobbies')).push(control);
@@ -36,5 +69,6 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.signupForm);
+    // this.signupForm.reset();
   }
 }
