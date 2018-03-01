a212edabfb7a79083b405a03bbf07fccccd687e6
sihong31 <sihongspring2011@gmail.com>
2018-02-28 22:54:48 -0500

    angular practice

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,41 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   signupForm: FormGroup;
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  forbiddenProjectNames = ['Test'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'projectName': new FormControl(null, Validators.required),
-      'email': new FormControl(null),
-      'projectStatus': new FormControl(null),
+      // 'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      'projectName': new FormControl(null, [Validators.required], this.forbiddenNames.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('Critical')
     })
   }
 
-  onSubmit() {
+  // forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  //   console.log(this.forbiddenProjectNames.indexOf(control.value))
+  //   if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+  //     return {'projectNameIsForbidden': true};
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
+  forbiddenNames(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "Test") {
+          resolve({'projectNameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
