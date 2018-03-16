45531b214b94dcda8ceda7e2b09fa954d54311f7
sihong31 <sihongspring2011@gmail.com>
2018-03-16 08:31:10 -0400

    angular practice

---
+++ my-shopping-app/src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShoppingListRoutingModule } from './shopping-list.routing.module';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    NgModule,
+    CommonModule
+  ]
+})
+
+export class ShoppingListModule {
+}
