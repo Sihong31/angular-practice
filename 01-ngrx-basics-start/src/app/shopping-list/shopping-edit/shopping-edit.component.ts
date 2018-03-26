ce108848552e0287da55c8b077d60d8e3e8b4c35
Sihong Huang <sihonghuang@bitbucket.org>
2018-03-26 16:19:57 -0400

    work on ngrx state

--- 01-ngrx-basics-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 01-ngrx-basics-start/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -45,7 +45,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient}));
     } else {
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
@@ -59,7 +59,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-    this.slService.deleteIngredient(this.editedItemIndex);
+  this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex));
     this.onClear();
   }
 
